fccea42d588897e19db7c673e330f67356e31af0
mekong777 <djruss77@gmail.com>
2026-09-24 14:49:13 -0400

    Add test case for add function with incorrect expected value
    Added a failing test case for the add function.

--- test_calculator.py
+++ test_calculator.py
@@ -7,6 +7,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
         self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(2, 3), 6)
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 2), 3)
